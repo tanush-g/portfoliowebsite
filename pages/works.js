cdd5c9fb7d3295838a96a39ac79c29632f52f7b6
Tanush <tanush.gautam16@gmail.com>
2024-04-22 05:56:24 +0530

    Update works.js to include a new WorkGridItem component and add ImageBlend project

--- pages/works.js
+++ pages/works.js
@@ -1,14 +1,31 @@
-import { Container, Box, Heading, SimpleGrid, Divider } from "@chakra-ui/react";
-import Section from "../components/section";
+import { Container, Heading, SimpleGrid, Divider } from "@chakra-ui/react"
+import Section from "../components/section"
+import { WorkGridItem } from "../components/grid-item"
+
+import thumbImageBlend from "../public/images/works/imageblend.png"
 
 const Works = () => {
-    return (
-        <Container>
-            <Heading as="h3" fontSize={20} mb={4}>
-                Works
-            </Heading>
-        </Container>
-    )
+  return (
+    <Container>
+      <Heading as="h3" fontSize={20} mb={4}>
+        Works
+      </Heading>
+
+      <SimpleGrid columns={[1, 1, 2]} gap={6}>
+        <Section>
+          <WorkGridItem
+            id="imageblend"
+            title="ImageBlend"
+            thumbnail={thumbImageBlend}
+          >
+            A simple image editor that allows you get the perfect photo every single time!
+          </WorkGridItem>
+        </Section>
+      </SimpleGrid>
+    </Container>
+
+    
+  )
 }
 
-export default Works;
+export default Works
