24b7cc3d2117670308c1d96588766ee3eabf4e0d
Tanush <tanush.gautam16@gmail.com>
2024-04-24 23:20:19 +0530

    Add new components: NoSsr and NotFound, and update existing components: model.js and main.js

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -1,25 +1,27 @@
-import Head from 'next/head'
-import Navbar from '../navbar.js'
-import { Box, Container } from '@chakra-ui/react'
-import SkaterBoy from '../3d-skater'
-
+import Head from "next/head"
+import Navbar from "../navbar"
+import NoSsr from "../no-ssr"
+import { Box, Container } from "@chakra-ui/react"
+import SkaterBoy from "../3d-skater"
 
 const Main = ({ children, router }) => {
-    return (
-        <Box as="main" pb={8}>
-            <Head>
-                <meta name="viewport" content="width=device-width, initial-scale=1" />
-                <title>Tanush Gautam - Homepage</title>
-            </Head>
+  return (
+    <Box as="main" pb={8}>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>Tanush Gautam - Homepage</title>
+      </Head>
 
-            <Navbar path={router.asPath} />
+      <Navbar path={router.asPath} />
 
-            <Container maxW="container.md" pt={14}>
-                <SkaterBoy />
-                {children}
-            </Container>
-        </Box>
-    )
+      <Container maxW="container.md" pt={14}>
+        <NoSsr>
+          <SkaterBoy />
+        </NoSsr>
+        {children}
+      </Container>
+    </Box>
+  )
 }
 
 export default Main
