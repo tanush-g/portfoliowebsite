de26c26d0f8e028b94326598dc52296f6f5e5e16
Tanush <tanush.gautam16@gmail.com>
2025-04-13 05:21:00 +0530

    feat: add Vercel Analytics and Speed Insights for enhanced performance tracking

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,8 @@ import Layout from "../components/layouts/main";
 import Fonts from "../components/fonts";
 import theme from "../lib/theme";
 import { AnimatePresence } from "framer-motion";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const Website = ({ Component, pageProps, router }) => {
   return (
@@ -13,6 +15,8 @@ const Website = ({ Component, pageProps, router }) => {
         <Component {...pageProps} key={router.route} />
         </AnimatePresence>
       </Layout>
+      <Analytics />
+      <SpeedInsights />
     </ChakraProvider>
   );
 }
