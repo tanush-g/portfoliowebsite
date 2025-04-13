833f469212ef4a41a5d505ae25bf4b193079ba86
Tanush <tanush.gautam16@gmail.com>
2025-04-13 15:27:58 +0530

    fix: correct typo in text-indent property in BioSection styling

--- components/bio.js
+++ components/bio.js
@@ -3,7 +3,7 @@ import styled from "@emotion/styled";
 
 export const BioSection = styled(Box)`
 padding-left: 3.4em;
-test-indent: -3.4em;
+text-indent: -3.4em;
 `;
 
 export const BioYear = styled.span`
